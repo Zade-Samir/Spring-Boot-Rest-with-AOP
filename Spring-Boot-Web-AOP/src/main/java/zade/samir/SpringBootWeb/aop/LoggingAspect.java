db06e68dabcdfe46c6d831cbd0fbbe77081fd83e
sbanduji <sbanduji@10.236.6.214>
2025-08-25 21:26:26 +0530

    Before Advice Applied

--- Spring-Boot-Web-AOP/src/main/java/zade/samir/SpringBootWeb/aop/LoggingAspect.java
+++ Spring-Boot-Web-AOP/src/main/java/zade/samir/SpringBootWeb/aop/LoggingAspect.java
@@ -1,12 +1,19 @@
 package zade.samir.SpringBootWeb.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+@Component
+@Aspect
 public class LoggingAspect {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 	
+	//return type, class name, method name(arguments)
+	@Before("execution(* zade.samir.SpringBootWeb.service.JobService.*(..))")
 	public void logMethodCall() {
 		System.out.println("Method called");
 	}
