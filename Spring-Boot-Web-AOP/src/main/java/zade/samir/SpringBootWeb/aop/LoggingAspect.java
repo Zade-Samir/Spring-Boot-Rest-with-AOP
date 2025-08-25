3463b21dbe6b3c98382a1679906ae0f261dde2d7
sbanduji <sbanduji@10.236.6.214>
2025-08-25 21:44:41 +0530

    Before, After and AfterThrowing is applied

--- Spring-Boot-Web-AOP/src/main/java/zade/samir/SpringBootWeb/aop/LoggingAspect.java
+++ Spring-Boot-Web-AOP/src/main/java/zade/samir/SpringBootWeb/aop/LoggingAspect.java
@@ -1,5 +1,9 @@
 package zade.samir.SpringBootWeb.aop;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -13,8 +17,37 @@ public class LoggingAspect {
 	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
 	
 	//return type, class name, method name(arguments)
-	@Before("execution(* zade.samir.SpringBootWeb.service.JobService.*(..))")
-	public void logMethodCall() {
-		System.out.println("Method called");
+	@Before("execution (* zade.samir.SpringBootWeb.service.JobService.getJob(..)) || zade.samir.SpringBootWeb.service.JobService.updateJob(..))")
+	public void logMethodCall(JoinPoint jp) {
+		LOGGER.info("Method Called "+jp.getSignature().getName());
 	}
+
+	@After("execution (* zade.samir.SpringBootWeb.service.JobService.getJob(..)) || zade.samir.SpringBootWeb.service.JobService.updateJob(..))")
+	public void logMethodExecuted(JoinPoint jp) {
+		LOGGER.info("Method Executed "+jp.getSignature().getName());
+	}
+	
+	@AfterThrowing("execution (* zade.samir.SpringBootWeb.service.JobService.getJob(..)) || zade.samir.SpringBootWeb.service.JobService.updateJob(..))")
+	public void logMethodCrashed(JoinPoint jp) {
+		LOGGER.info("Method has some issues "+jp.getSignature().getName());
+	}
+	
+	@AfterReturning("execution (* zade.samir.SpringBootWeb.service.JobService.getJob(..)) || zade.samir.SpringBootWeb.service.JobService.updateJob(..))")
+	public void logMethodExecutedSuccess(JoinPoint jp) {
+		LOGGER.info("Method Executed Successfully "+jp.getSignature().getName());
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
